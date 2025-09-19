6fa2ffe452a104744693f590983fdd15d19afa3f
elomokm <elom.okoumassoun@epitech.eu>
2025-09-19 10:53:47 +0200

    re init project

--- web/src/pages/News.tsx
+++
@@ -1,8 +0,0 @@
-export default function News() {
-  return (
-    <div className="space-y-2">
-      <h2 className="text-xl font-semibold">Actualités</h2>
-      <p className="text-gray-600">Bientôt: table des actus depuis l’API.</p>
-    </div>
-  )
-}
